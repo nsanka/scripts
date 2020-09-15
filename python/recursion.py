713916bb734ee533b956119934e959e35a4f8ef4
Naga Sanka <nsanka@me.com>
2020-09-15 09:05:17 -0400

    Create recursion.py

---
+++ python/recursion.py
@@ -0,0 +1,42 @@
+class Node(object):
+  def __init__(self, value, left=None, right=None):
+    self.value = value
+    self.left = left
+    self.right = right
+
+class Solution(object):
+  def _preorder_helper(self, n, ret):
+    if not n:
+      return
+    ret += str(n.value)
+    self._preorder_helper(n.left, ret)
+    self._preorder_helper(n.right, ret)
+
+  def preorder(self, n):
+    ret = []
+    self._preorder_helper(n, ret)
+    return ''.join(ret)
+
+  def preorder_iterative(self, n):
+    stack = [n]
+    ret = []
+    while stack:
+      node = stack.pop()
+      if not node:
+        continue
+      ret += str(node.value)
+      stack.append(node.right)
+      stack.append(node.left)
+    return ''.join(ret)
+
+#       1
+#      / \
+#     2   5
+#    / \
+#   3   4
+n = Node(1, Node(2, Node(3), Node(4)), Node(5))
+print(Solution().preorder(n))
+# 12345
+
+print(Solution().preorder_iterative(n))
+# 12345
